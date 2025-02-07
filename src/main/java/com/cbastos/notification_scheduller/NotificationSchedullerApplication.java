08439f5f53b29b53704ab393edd452ef344e7cf4
CesarSBastos <cesarbastos32@gmail.com>
2025-02-06 21:09:05 -0300

    initialize notification-scheduller project with Spring Boot setup and necessary configurations

---
+++ src/main/java/com/cbastos/notification_scheduller/NotificationSchedullerApplication.java
@@ -0,0 +1,13 @@
+package com.cbastos.notification_scheduller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSchedullerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSchedullerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbastos/notification_scheduller/NotificationSchedullerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbastos.notification_scheduller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSchedullerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
